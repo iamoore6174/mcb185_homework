5004cc6dfbbed86597d2a8071a735430930757e3
Iris Moore <moore.a.iris@gmail.com>
2026-01-27 15:19:56 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,18 @@
+# 10demo.py by Iris Moore
+
+print ('hello, again') # greeting
+
+print(1.5e-2)
+
+print(2**3) # 2**3= 2^3 
+
+print(5//2) # integer division= ignores remainder 
+
+print(5%2) # gives only remainder
+
+print(pow(2,3)) # x to the power of y
+
+import math
+
+print(math.e)
+print(math.factorial(10))

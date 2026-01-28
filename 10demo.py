933b47b301ce6f17b3613a33560f9e81fb4d721f
Iris Moore <moore.a.iris@gmail.com>
2026-01-27 18:16:09 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -16,3 +16,44 @@ import math
 
 print(math.e)
 print(math.factorial(10))
+
+# Write function is_prob(x) that determines boolean if a number is a valid probability x>=0, x<=1 
+
+def is_prob(x):
+    return x>=0 and x<=1
+print(is_prob(0.5)) 
+
+# Write function distance(x1, y1, x2, y2) that computes cartesian distance between two points on a graph 
+
+# def (distance(x1, y1, x2, y2))
+   #return math.sqrt((x2 -x1)**2 + (y2 -y1)**2)
+# print(distance())
+
+# 19) fibonacci, print out first 100 numbers of fibonacci seq
+# 1 1 2 3 5 8 13 21
+
+a=1
+b=1
+fib = a + b 
+print(fib)
+
+while True:
+    a = b
+    b = fib
+    fib= a + b 
+    print(fib)
+    if fib > 100: break
+
+# a = 5 b = 3, how can you swap the variables? need additional variable to swap
+# a = ...
+# b = ...
+# c = a 
+# a = 
+
+ def min4(a, b, c, d ):
+    if a <= b and a <= c and a <= d: return a 
+    if b <= c and b <= d: return b # stepwise, use less than for all
+    if c <= d: return c 
+    return d 
+
+print (min4(5, 3, -1, 0))   

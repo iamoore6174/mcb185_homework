cc154b77b8f73a2d8532253831337db6b5f061bb
Iris Moore <moore.a.iris@gmail.com>
2026-01-28 16:35:25 -0800

    update

--- 11oligo.py
+++ 11oligo.py
@@ -1,7 +1,9 @@
+# 11oligo.py by Iris Moore
 # Write a function that returns the oligo melting temperature given the number of As, Cs, Gs, and Ts in the oligo. 
 # Use these two methods.
 
-# For oligos <= 13 nt, Tm = (A+T)*2 + (G+C)*4
+# For oligos <= 13 nt, Tm = (A+T)*2 + (G+C)*4 
+# For longer oligos, Tm = 64.9 + 41*(G+C -16.4) / (A+T+G+C)
 
 def tm(A, T, C, G):
     total = (A + T + C + G) # make variable for adding number of nts
